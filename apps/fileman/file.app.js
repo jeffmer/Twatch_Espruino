24b0a1d7fc01599e5d4a241ba258024c97847cb3
jeffmer <jeffmer@users.noreply.github.com>
2020-10-11 12:20:44 +0100

    added app manager

--- apps/fileman/file.app.js
+++ apps/fileman/file.app.js
@@ -1,102 +1,229 @@
-const STOR = require("Storage");
-E.showMenu = eval(STOR.read("menu.js"));
+const store = require('Storage');
+E.showMenu = eval(store.read("menu.js"));
 
-const n = 9;
-var nstart = 0;
-var nend;
-var m;
-var files;
+const boolFormat = (v) => v ? "On" : "Off";
 
-function delete_file(fn) {
-  E.showPrompt("Delete\n"+fn+"?", {buttons: {"No":false, "Yes":true}}).then(function(v) {
-    if (v) {
-      if (fn.charCodeAt(fn.length-1)==1) {
-        var fh = STOR.open(fn.substr(0, fn.length-1), "w");
-        fh.erase();
+function showMainMenu() {
+  const mainmenu = {
+    '': {
+      'title': 'App Manager',
+    },
+    'Free': {
+      value: undefined,
+      format: (v) => {
+        return store.getFree();
+      },
+      onchange: () => {}
+    },
+    'Compact': () => {
+      E.showMessage('Compacting...');
+      try {
+        store.compact();
+      } catch (e) {
       }
-      else STOR.erase(fn);
-    }
-  }).then(function() { files=get_pruned_file_list(); }).then(drawMenu);
-}
-
-function get_length(fn) {
-  var len;
-  if (fn.charCodeAt(fn.length-1)==1) {
-    var fh = STOR.open(fn.substr(0, fn.length-1), "r");
-    len = fh.getLength();
-  }
-  else len = STOR.read(fn).length;
-  return len;
-}
-
-function display_file(fn, qJS) {
-  g.clear().setColor(1, 1, 1);
-  var qStorageFile = (fn.charCodeAt(fn.length-1)==1);
-  var np = 0;
-  Terminal.println("");
-  var file_len = get_length(fn);
-  var fb = (qStorageFile ? STOR.open(fn.substr(0, fn.length-1), "r") : STOR.read(fn));
-  for (var i=0; i<file_len; ++i) {
-    if (np++ > 38) {
-      Terminal.println("");
-      np = 0;
-    }
-    var c = (qStorageFile ? fb.read(1) : fb[i]);
-    if (c=="\n") np = 0;
-    if (qJS && !qStorageFile && c==";" && fb[i+1]!="\n") {
-      Terminal.println(";");
-      np = 0;
-    }
-    else Terminal.print(c);
-  }
-  Terminal.println("");
-}
-
-function visit_file(fn) {
-  var menu = {
-    '' : {'title' : fn + (fn.charCodeAt(fn.length-1)==1 ? "(S)" : "")}
+      showMainMenu();
+    },
+    'Apps': ()=> showApps(),
+    'Sort Apps': () => showSortAppsMenu(),
+    '< Back': ()=> {load("launch.js");}
   };
-  var qJS = fn.endsWith(".js");
-  menu['Length: '+get_length(fn)+' bytes'] = function() {};
-  menu['Display file'] = function () { display_file(fn, qJS); };
-  if (qJS && !fn.endsWith(".wid.js")) menu['Load file'] = function() { load(fn); }
-  if (fn.endsWith(".img")) menu['Display image'] = function() { g.clear().drawImage(STOR.read(fn),0,20); }
-  menu['Delete file'] = function () { delete_file(fn); }
-  menu['< Back'] = drawMenu;
-  E.showMenu(menu);
+  E.showMenu(mainmenu);
 }
 
-function drawMenu() {
-  nend = (nstart+n<files.length)?nstart+n : files.length;
-  var menu = {
-    '': { 'title': 'Dir('+nstart+'-'+nend+')/'+files.length }
+function isGlob(f) {
+  return /[?*]/.test(f);
+}
+
+function globToRegex(pattern) {
+  const ESCAPE = '.*+-?^${}()|[]\\';
+  const regex = pattern.replace(/./g, c => {
+    switch (c) {
+      case '?': return '.';
+      case '*': return '.*';
+      default: return ESCAPE.includes(c) ? ('\\' + c) : c;
+    }
+  });
+  return new RegExp('^'+regex+'$');
+}
+
+function eraseFiles(info) {
+  info.files.split(",").forEach(f=>store.erase(f));
+}
+
+function eraseData(info) {
+  if(!info.data) return;
+  const d=info.data.split(';'),
+    files=d[0].split(','),
+    sFiles=(d[1]||'').split(',');
+  let erase = f=>store.erase(f);
+  files.forEach(f=>{
+    if (!isGlob(f)) erase(f);
+    else store.list(globToRegex(f)).forEach(erase);
+  });
+  erase = sf=>store.open(sf,'r').erase();
+  sFiles.forEach(sf=>{
+    if (!isGlob(sf)) erase(sf);
+    else store.list(globToRegex(sf+'\u0001'))
+      .forEach(fs=>erase(fs.substring(0,fs.length-1)));
+  });
+}
+function eraseApp(app, files,data) {
+  E.showMessage('Erasing\n' + app.name + '...');
+  var info = store.readJSON(app.id + ".info", 1)||{};
+  if (files) eraseFiles(info);
+  if (data) eraseData(info);
+}
+function eraseOne(app, files,data){
+  E.showPrompt('Erase\n'+app.name+'?').then((v) => {
+    if (v) {
+      Bangle.buzz(100, 1);
+      eraseApp(app, files, data);
+      showApps();
+    } else {
+      showAppMenu(app);
+    }
+  });
+}
+function eraseAll(apps, files,data) {
+  E.showPrompt('Erase all?').then((v) => {
+    if (v) {
+      Bangle.buzz(100, 1);
+      apps.forEach(app => eraseApp(app, files, data));
+    }
+    showApps();
+  });
+}
+
+function showAppMenu(app) {
+  let appmenu = {
+    '': {
+      'title': app.name,
+    },
+    '< Back': () => showApps(),
   };
-  menu["< prev"] = function() {
-    nstart -= n;
-    if (nstart<0) nstart = files.length-n>0 ? files.length-n : 0;
-    menu = {};
-    drawMenu();
+  if (app.hasData) {
+    appmenu['Erase Completely']    = () => eraseOne(app, true, true);
+    appmenu['Erase App,Keep Data'] = () => eraseOne(app, true, false);
+    appmenu['Only Erase Data']     = () => eraseOne(app, false, true);
+  } else {
+    appmenu['Erase'] = () => eraseOne(app, true, false);
   }
-  for (var i=nstart; i<nend; ++i) {
-    menu[files[i]] = visit_file.bind(null, files[i]);
-  }
-  menu["> next"] = function() {
-    if (nstart+n<files.length) nstart += n;
-    else nstart = 0;
-    menu = {};
-    drawMenu();
-    m.move(-1);
-  }
-  m = E.showMenu(menu);
+  E.showMenu(appmenu);
 }
 
-function get_pruned_file_list() {
-  var fl = STOR.list(/^[^\.]/);
-  fl.sort();
-  fl = fl.concat(STOR.list(/^\./));
-  fl = fl.filter(f => (f.charCodeAt(f.length-1)>31 || f.charCodeAt(f.length-1)<2));
-  return fl;
+function showApps() {
+  const appsmenu = {
+    '': {
+      'title': 'Apps',
+    },
+    '< Back': () => showMainMenu(),
+  };
+
+  var list = store.list(/\.info$/).filter((a)=> {
+    return a !== 'setting.info';
+  }).map((a)=> {
+    let app = store.readJSON(a, 1) || {};
+    return {id: app.id, name: app.name, hasData: !!app.data};
+  }).sort(sortHelper());
+
+  if (list.length > 0) {
+    list.reduce((menu, app) => {
+      menu[app.name] = () => showAppMenu(app);
+      return menu;
+    }, appsmenu);
+    appsmenu['Erase All'] = () => {
+      E.showMenu({
+        '': {'title': 'Erase All'},
+        'Erase Everything':     () => eraseAll(list, true, true),
+        'Erase Apps,Keep Data': () => eraseAll(list, true, false),
+        'Only Erase Data':      () => eraseAll(list, false, true),
+        '< Back': () => showApps(),
+      });
+    };
+  } else {
+    appsmenu['...No Apps...'] = {
+      value: undefined,
+      format: ()=> '',
+      onchange: ()=> {}
+    };
+  }
+  E.showMenu(appsmenu);
 }
 
-files = get_pruned_file_list();
-setTimeout(drawMenu,500);
+function showSortAppsMenu() {
+  const sorterMenu = {
+    '': {
+      'title': 'App Sorter',
+    },
+    '< Back': () => showMainMenu(),
+    'Sort: manually': ()=> showSortAppsManually(),
+    'Sort: alph. ASC': () => {
+      E.showMessage('Sorting:\nAlphabetically\nascending ...');
+      sortAlphabet(false);
+    },
+    'Sort: alph. DESC': () => {
+      E.showMessage('Sorting:\nAlphabetically\ndescending ...');
+      sortAlphabet(true);
+    }
+  };
+  E.showMenu(sorterMenu);
+}
+
+function showSortAppsManually() {
+  const appsSorterMenu = {
+    '': {
+      'title': 'Sort: manually',
+    },
+    '< Back': () => showSortAppsMenu(),
+  };
+  let appList = getAppsList();
+  if (appList.length > 0) {
+    appList.reduce((menu, app) => {
+      menu[app.name] = {
+        value: app.sortorder || 0,
+        min: 0,
+        max: appList.length,
+        step: 1,
+        onchange: val => setSortorder(app, val)
+      };
+      return menu;
+    }, appsSorterMenu);
+  } else {
+    appsSorterMenu['...No Apps...'] = {
+      value: undefined,
+      format: ()=> '',
+      onchange: ()=> {}
+    };
+  }
+  E.showMenu(appsSorterMenu);
+}
+
+function setSortorder(app, val) {
+  app = store.readJSON(app.id + '.info', 1);
+  app.sortorder = val;
+  store.write(app.id + '.info', JSON.stringify(app));
+}
+
+function getAppsList() {
+  return store.list('.info').map((a)=> {
+    let app = store.readJSON(a, 1) || {};
+    if (app.type !== 'widget') {
+      return {id: app.id, name: app.name, sortorder: app.sortorder};
+    }
+  }).filter((a) => a).sort(sortHelper());
+}
+
+function sortAlphabet(desc) {
+  let appsSorted = desc ? getAppsList().reverse() : getAppsList();
+  appsSorted.forEach((a, i) => {
+    setSortorder(a, i);
+  });
+  showSortAppsMenu();
+}
+
+function sortHelper() {
+  return (a, b) => (a.name > b.name) - (a.name < b.name);
+}
+
+setTimeout(showMainMenu,500);
+
